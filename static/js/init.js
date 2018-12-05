766395ab8521162363bc65d0d6f9154d1191fe8f
chenhuachao <chenhuachao@163.com>
2018-12-05 15:50:22 +0800

    home fix

---
+++ static/js/init.js
@@ -0,0 +1,27 @@
+ layui.use('jquery', function(){
+   $ = layui.jquery;
+});
+    // 点击按钮，返回顶部
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function zaixianliuyan() {
+    var liuyan=document.getElementById("zaixian_liuyan");
+    liuyan.style.display="block";
+}
+function closeDiv() {
+    var liuyan=document.getElementById("zaixian_liuyan");
+    liuyan.style.display="none";
+}
+function zhedie() {
+  $("#chc-cate").animate({right:"0%"});
+}
+document.addEventListener("click", function(e){
+     // 判断被点击的元素是不是scheduleInput元素，不是的话，就隐藏之
+    var cateleft = document.getElementById("zhedieid");
+    console.log(e.target);
+     if( e.target !== cateleft ){
+           $("#chc-cate").animate({right:"-59%"});
+     }
+});
