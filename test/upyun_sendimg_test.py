035d10a98d65f3ef5ab11435cc9fd50c2dc8ac75
Chou <shoringchow@foxmail.com>
2018-12-11 21:19:06 +0800

    新增又拍云图片推送脚本

---
+++ test/upyun_sendimg_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/12/11 上午9:41
+# @Author  : ZSG
+# @Email   : 505972916@qq.com
+# @File    : upyun_sendimg_test.py
+# @Software: PyCharm
+import upyun
+import time
+import requests
+
+UPYUN_BUCKETNAME = 'doors'
+UPYUN_USERNAME = 'doors360'
+UPYUN_PASSWORD = 'doors360.cn'
+UPYUN_BASE_URL = "https://upyun.doors360.cn"
+
+def upload_img(_imgdata,extname):
+    def up_to_upyum(key, value):
+        up_conn = upyun.UpYun(UPYUN_BUCKETNAME, UPYUN_USERNAME, UPYUN_PASSWORD)
+        up_headers = {}
+        up_conn.put(key, value, checksum=True, headers=up_headers)
+        return UPYUN_BASE_URL + key
+    try:
+        _file = _imgdata
+        filename = str(time.time()).replace('.', '_') + extname
+        image_key = "/image_upload/" + filename
+        url = up_to_upyum(image_key, _file)
+        return {"status": True, "filename": filename, "imgurl": url}
+    except Exception:
+        return {"status": False, "message": 'Err'}
+
+def Transfer(_url):
+    response = requests.get(_url)
+    _imgdata = response.content
+    extname="." + _url.split(".")[-1].split("?")[0]
+    if "/" in extname:
+        extname = ".jpg"
+    new_name = upload_img(_imgdata, extname)
+    print(new_name)
+
+_url="http://www.threebao.com/uploadfile/2018/0319/20180319104845203.jpg"
+Transfer(_url)
