596f73efbda578bc8fed004fdbbc9d18493df148
chenhuachao <chenhuachao@163.com>
2018-12-14 14:03:23 +0800

    sql update

--- apps/setting.py
+++ apps/setting.py
@@ -8,4 +8,9 @@
 # password = ''
 # port = 3306
 # db_name = ''
-sqlurl = 'mysql+pymysql://root:123456@127.0.0.1:3306/doors'
+import socket
+# myname = socket.getfqdn(socket.gethostname())
+# myaddr = socket.gethostbyname(myname)
+# print(myaddr)
+#sqlurl = 'mysql+pymysql://root:123456@127.0.0.1:3306/doors'
+sqlurl = 'mysql+pymysql://mha_user:gc895316@127.0.0.1:3306/doors'
